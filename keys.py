9130688108fc2e0396a34e64e62ac0aa8c521ffe
Juliano <jnds2197@gmail.com>
2024-04-12 20:05:54 -0300

    first commit

---
+++ keys.py
@@ -0,0 +1,33 @@
+###Twitter
+
+#keys
+
+twitter_api_key = ''
+
+twitter_api_key_secret = ''
+
+twitter_bearer_token = ''
+
+twitter_access_token = ''
+
+twitter_access_token_secret = ''
+
+#OAuth 2.0:
+
+twitter_client_id = ''
+
+twitter_client_seeker = ''
+
+###Twitch:
+
+#Authentication:
+
+twitch_client_id = ''
+
+twitch_client_secret = ''
+
+twitch_server = 'irc.chat.twitch.tv'
+twitch_port = 6667
+twitch_nickname = 'focken_ediot'
+twitch_token = ''
+twitch_channel = '#sennyk4'
---
+++ main.py
@@ -0,0 +1,77 @@
+import keys
+import tweepy
+import socket
+from emoji import demojize
+
+def main():
+    #V2 authentication, for text tweets
+    client = tweepy.Client(
+                consumer_key=keys.twitter_api_key,
+                consumer_secret=keys.twitter_api_key_secret,
+                access_token=keys.twitter_access_token,
+                access_token_secret=keys.twitter_access_token_secret
+            )
+
+    #V1, for tweeting with images
+    """
+    auth = tweepy.OAuth1UserHandler(
+        keys.twitter_api_key,
+        keys.twitter_api_key_secret,
+        keys.twitter_access_token,
+        keys.twitter_access_token_secret,
+    )
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    """
+
+    #Socket connection to Twitch
+    sock = socket.socket()
+
+    sock.connect((keys.twitch_server, keys.twitch_port))
+
+    sock.send(f"PASS {keys.twitch_token}\n".encode('utf-8'))
+    sock.send(f"NICK {keys.twitch_nickname}\n".encode('utf-8'))
+    sock.send(f"JOIN {keys.twitch_channel}\n".encode('utf-8'))
+
+    """
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s — %(message)s',
+                        datefmt='%Y-%m-%d_%H:%M:%S',
+                        handlers=[logging.FileHandler('chat.log', encoding='utf-8')])
+    """
+
+    #Listener
+    while True:
+        response = sock.recv(2048).decode('utf-8')
+
+        if response.startswith('PING'):
+            sock.send("PONG\n".encode('utf-8'))
+        
+        elif len(response) > 0:
+            #logging.info(demojize(resp))
+
+            response = str(demojize(response)).rstrip()
+            username = response.split(':')[1:][0].split('!')[0]
+
+            if(username == "sennyk4"):
+                message = response.split(':')[1:][1]
+                #print(username)
+                #print(message)
+                client.create_tweet(text = message)
+                print("Tweeted successfully")
+
+    """
+    def tweet(message: str, image_path = None):
+        if image_path:
+            media = api.media_upload(image_path)
+            client.create_tweet(text = message, media_ids=[media.media_id])
+            print("Tweeted successfully")
+
+        else:      
+            client.create_tweet(text = message)
+            print("Tweeted successfully")
+
+    """
+
+
+if __name__ == '__main__':
+    main()
